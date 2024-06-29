ac43f2906dadedef7427fe75db85cdebbf4b7218
kirlos basta <kirlosbasta@gmail.com>
2024-06-29 05:33:47 +0300

    clean 0-pascal_tringle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,13 +7,17 @@ def pascal_triangle(n):
     the Pascal’s triangle of n'''
     if n <= 0:
         return []
+    # the first row is always the same 1
     trinagle = [[1]]
     for i in range(1, n):
         row = []
+        # add the first number 1
         row.append(1)
         for j in range(1, i):
+            # the num is the sum of the above two numbers
             num = trinagle[i - 1][j - 1] + trinagle[i - 1][j]
             row.append(num)
+        # add the last number 1
         row.append(1)
         trinagle.append(row)
     return trinagle
