761778222ce4a4d7e7050fc4e46825935bcd2217
kirlos basta <kirlosbasta@gmail.com>
2024-07-05 03:29:31 +0300

    Add 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,12 +5,15 @@ from typing import List
 
 def canUnlockAll(boxes: List[List[int]]) -> bool:
     '''Check if all the boxes can be unlocked'''
-    if len(boxes) == 0:
+    boxes_len = len(boxes)
+    if boxes_len == 0:
         return False
     visited = set([0])
     stack: List[int] = [*boxes[0]]
     while (len(stack) != 0):
         key = stack.pop()
+        if key > boxes_len - 1:
+            continue
         if key in visited:
             continue
         visited.add(key)
